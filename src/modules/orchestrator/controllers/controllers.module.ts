9552450696adbdc8f788948de204ceb2a143f79f
OmerCohen993 <96525811+OmerCohen993@users.noreply.github.com>
2025-08-07 20:29:58 +0300

    Update controllers.module.ts

--- src/modules/orchestrator/controllers/controllers.module.ts
+++ src/modules/orchestrator/controllers/controllers.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { OrchestratorServiceModule } from '../service/orchestrator-service.module';
-import { OrchestratorService } from '../service/orchestrator.service';
 import { StringCompressionServiceModule } from '../../string-compression/service/string-compression-service.module';
 import { StringUpperClassServiceModule } from '../../string-upper-class/service/string-upper-class-service.module';
 
 @Module({
   imports: [OrchestratorServiceModule, StringCompressionServiceModule, StringUpperClassServiceModule],
   controllers: [TaskController],
-  providers: [OrchestratorService],
 })
 export class ControllersModule {} 
